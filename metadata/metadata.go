c043eb1679c0b190c220728070e958680f4af55c
Roman Yaroshenko <roma.yaroshenko@gmail.com>
2014-07-07 16:45:21 +0300

    [+] Доработана структура метаданных

--- metadata/metadata.go
+++ metadata/metadata.go
@@ -3,27 +3,49 @@ package metadata
 
 import (
 	"dshop/datetime"
+	"dshop/uuid"
 )
+
+const (
+	MetaVersion = uint16(1)
+)
+
+type Meta struct {
+	Name      string
+	Version   uint16
+	ItemCount uint32
+}
 
 // Shop - структура "Магазин"
 type Shop struct {
-	UID         string
+	UID         uuid.UUID
 	Name        string
 	Description string
 }
 
+type ShopList struct {
+	MetaInfo Meta
+	Items    []Shop
+}
+
 // Tran - структура "Транзакция"
 type Tran struct {
-	ShopUID    string
+	ShopUID    uuid.UUID
 	TranTime   datetime.DateTime
-	PackageUID string
+	PackageUID uuid.UUID
 }
 
 // Goods - структура "Товар"
 type Goods struct {
-	UID       string
+	UID       uuid.UUID
 	Article   string
 	ShortName string
 	Name      string
+	Shared    bool
 	TranInfo  Tran
+}
+
+type GoodsList struct {
+	MetaInfo Meta
+	Items    []Goods
 }

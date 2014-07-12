fc47581e6e2b8816ba457abf38b161817814a79b
Roman Yaroshenko <roma.yaroshenko@gmail.com>
2014-07-12 17:12:06 +0300

    [+] добавлены к типу commands функция создания New() и функция обработки команд run()

--- safemap/safemap.go
+++ safemap/safemap.go
@@ -5,10 +5,10 @@ type UpdateFunc func(interface{}, bool) interface{}
 type SafeMap interface {
 	Insert(string, interface{})
 	Update(string, UpdateFunc) (interface{}, bool)
-	Delete(string)
+	Remove(string)
 	Find(string) (interface{}, bool)
-	Len() int
-	Close()
+	Length() int
+	Done()
 }
 
 type findresult struct {
@@ -21,7 +21,7 @@ type commandaction int
 const (
 	insert commandaction = iota
 	update
-	delete
+	remove
 	find
 	length
 	done
@@ -48,8 +48,8 @@ func (cs commands) Update(key string, updater UpdateFunc) (interface{}, bool) {
 	return result.value, result.found
 }
 
-func (cs commands) Delete(key string) {
-	cs <- command{action: delete, key: key}
+func (cs commands) Remove(key string) {
+	cs <- command{action: remove, key: key}
 }
 
 func (cs commands) Find(key string) (interface{}, bool) {
@@ -59,12 +59,43 @@ func (cs commands) Find(key string) (interface{}, bool) {
 	return result.value, result.found
 }
 
-func (cs commands) Len() int {
+func (cs commands) Length() int {
 	reply := make(chan interface{})
 	cs <- command{action: length, result: reply}
 	return (<-reply).(int)
 }
 
-func (cs commands) Close() {
+func (cs commands) Done() {
 	cs <- command{action: done}
+}
+
+func (cs commands) run() {
+	store := make(map[string]interface{})
+
+	for cmd := range cs {
+		switch cmd.action {
+		case insert:
+			store[cmd.key] = cmd.value
+		case update:
+			value, found := store[cmd.key]
+			new_value := cmd.updater(value, found)
+			store[cmd.key] = new_value
+			cmd.result <- findresult{new_value, found}
+		case remove:
+			delete(store, cmd.key)
+		case find:
+			value, found := store[cmd.key]
+			cmd.result <- findresult{value, found}
+		case length:
+			cmd.result <- len(store)
+		case end:
+			close(cs)
+		}
+	}
+}
+
+func New() SafeMap {
+	cs := make(commands)
+	go cs.run()
+	return cs
 }
